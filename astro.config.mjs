afaef5cf3e01abc0e465de72a3a7a7145bb0885b
bdhhjxjx67-ux <bdhhjxjx67@gmail.com>
2026-09-01 16:41:48 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://bdhhjxjx67-ux.github.io',
-  base: '/bestinfobooks-8687info/',
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
